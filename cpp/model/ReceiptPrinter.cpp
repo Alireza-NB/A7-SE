81fd5bfedcfb236924af9983a4f1de062d740489
Alireza-NB <alirezasharifpour79@gmail.com>
2022-01-08 23:27:42 +0330

    transfer implementation of ReceitPrinter to cpp file

--- cpp/model/ReceiptPrinter.cpp
+++ cpp/model/ReceiptPrinter.cpp
@@ -1,1 +1,68 @@
 #include "ReceiptPrinter.h"
+
+ReceiptPrinter() : ReceiptPrinter(40)
+{
+}
+
+ReceiptPrinter(int columns) : columns(columns)
+    {}
+
+    std::string printReceipt(Receipt* receipt)
+    {
+        std::string result;
+        for (const auto &item : receipt->getItems())
+        {
+            result.append(presentReceiptItem(item));
+        }
+        for (const auto &discount : receipt->getDiscounts())
+        {
+            result.append(presentDiscount(discount));
+        }
+        result.append("\n");
+        result.append(presentTotal(receipt));
+        return result;
+    }
+
+    std::string presentReceiptItem(ReceiptItem* item) const
+    {
+        std::string price = getFormattedNumberAsString(item->getTotalPrice(), 2);
+        std::string name = item->getProduct()->getName();
+
+        std::string line = formatLineWithWhitespace(name, price);
+
+        if (item->getQuantity() != 1)
+        {
+            line += "  " + getFormattedNumberAsString(item->getPrice(), 2) + " * " + presentQuantity(item) + "\n";
+        }
+        return line;
+    }
+
+    std::string presentDiscount(Discount* discount) const
+    {
+        std::string name = discount->getDescription() + "(" + discount->getProduct()->getName() + ")";
+        std::string pricePresentation = getFormattedNumberAsString(discount->getDiscountAmount(), 2);
+        return formatLineWithWhitespace(name, pricePresentation);
+    }
+
+    std::string presentTotal(Receipt* receipt) const
+    {
+        std::string total = "Total: ";
+        std::string pricePresentation = presentPrice(receipt->getTotalPrice());
+        return formatLineWithWhitespace(total, pricePresentation);
+    }
+
+    std::string formatLineWithWhitespace(const std::string &name, const std::string &value) const
+    {
+        int whitespaceSize = columns - name.length() - value.length();
+        std::string whitespace;
+//        char whitespace[whitespaceSize];
+        for (int i = 0; i < whitespaceSize; i++)
+        {
+            whitespace.append(" ");
+//            whitespace[i] = " ";
+        }
+        return name + whitespace + value + "\n";
+    }
+
+    std::string presentPrice(double price) const
+    { return getFormattedNumberAsString(price, 2); }
--- cpp/model/ReceiptPrinter.h
+++ cpp/model/ReceiptPrinter.h
@@ -11,72 +11,21 @@ class ReceiptPrinter
 {
 
 public:
-    ReceiptPrinter() : ReceiptPrinter(40)
-    {
-    }
+    ReceiptPrinter();
 
-    ReceiptPrinter(int columns) : columns(columns)
-    {}
+    ReceiptPrinter(int columns);
 
-    std::string printReceipt(Receipt* receipt)
-    {
-        std::string result;
-        for (const auto &item : receipt->getItems())
-        {
-            result.append(presentReceiptItem(item));
-        }
-        for (const auto &discount : receipt->getDiscounts())
-        {
-            result.append(presentDiscount(discount));
-        }
-        result.append("\n");
-        result.append(presentTotal(receipt));
-        return result;
-    }
+    std::string printReceipt(Receipt* receipt);
 
-    std::string presentReceiptItem(ReceiptItem* item) const
-    {
-        std::string price = getFormattedNumberAsString(item->getTotalPrice(), 2);
-        std::string name = item->getProduct()->getName();
+    std::string presentReceiptItem(ReceiptItem* item) const;
 
-        std::string line = formatLineWithWhitespace(name, price);
+    std::string presentDiscount(Discount* discount) const;
 
-        if (item->getQuantity() != 1)
-        {
-            line += "  " + getFormattedNumberAsString(item->getPrice(), 2) + " * " + presentQuantity(item) + "\n";
-        }
-        return line;
-    }
+    std::string presentTotal(Receipt* receipt) const;
 
-    std::string presentDiscount(Discount* discount) const
-    {
-        std::string name = discount->getDescription() + "(" + discount->getProduct()->getName() + ")";
-        std::string pricePresentation = getFormattedNumberAsString(discount->getDiscountAmount(), 2);
-        return formatLineWithWhitespace(name, pricePresentation);
-    }
+    std::string formatLineWithWhitespace(const std::string &name, const std::string &value) const;
 
-    std::string presentTotal(Receipt* receipt) const
-    {
-        std::string total = "Total: ";
-        std::string pricePresentation = presentPrice(receipt->getTotalPrice());
-        return formatLineWithWhitespace(total, pricePresentation);
-    }
-
-    std::string formatLineWithWhitespace(const std::string &name, const std::string &value) const
-    {
-        int whitespaceSize = columns - name.length() - value.length();
-        std::string whitespace;
-//        char whitespace[whitespaceSize];
-        for (int i = 0; i < whitespaceSize; i++)
-        {
-            whitespace.append(" ");
-//            whitespace[i] = " ";
-        }
-        return name + whitespace + value + "\n";
-    }
-
-    std::string presentPrice(double price) const
-    { return getFormattedNumberAsString(price, 2); }
+    std::string presentPrice(double price) const;
 
     static std::string presentQuantity(ReceiptItem* item)
     {
